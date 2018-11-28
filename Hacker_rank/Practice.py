76c43ea26404e305161918741a6d70fee7165e86
unixonboard <unixonboard@gmail.com>
2018-11-27 22:09:24 -0800

    .

---
+++ Hacker_rank/Practice.py
@@ -0,0 +1,38 @@
+# print("hello")
+
+def sample():
+    print("i'll print this no matter")
+    return
+sample()
+
+def calculator(a,b):
+    # var=a+b
+    return a+b
+sum=calculator(10,5)
+print(sum)
+
+employee={'thakur':1,'priya':2}
+def test(emp):
+    new={'nandu':3}
+    print("value",new)
+    return new
+funcvalue=test(employee)
+print("out func value", funcvalue)
+print("new value",employee)
+
+def test(a):
+    a=a+10
+    return
+a=10
+test(a)
+print(a)
+
+Global Variable update inside the function
+twx=100
+def test():
+    global twx
+    twx=twx+10
+    print(twx)
+    return
+test()
+print(twx)
