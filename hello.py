09211d1e71850e1e8189c0437fa66e50eb86c95a
Thakur Nanda <thakur_nanda@apple.com>
2020-04-14 11:09:55 -0500

    penv

--- hello.py
+++ hello.py
@@ -1,8 +1,27 @@
-def hello_func():
-    return 'Hello Function'
+# def hello_func():
+#     return 'Hello Function'
+#
+# print(hello_func())
+#
+# print("Hello,", name)
+# print("Hello Python")
+# name = input("Your Name? ")
+# import sys
+#
+# print(sys.version)
+# print(sys.executable)
 
-print(hello_func())
+import os
 
+if os.getenv('VIRTUAL_ENV'):
+    print('Using Virtualenv')
+else:
+    print('Not using Virtualenv')
 
-name = input("Your Name? ")
-print("Hello,", name)
+def sum(a,b):
+    return a+b
+n1=input('Enter first Number: ')
+n1=int(n1)
+n2=input('Enter the second Number: ')
+n2=int(n2)
+print('Sum is : ', sum(n1,n2))
